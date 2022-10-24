a8f1df2590d2d29671fa1a27d461fc56b77433fd
LuisMathues-dev <luismatheusbs.2016@gmail.com>
2022-10-24 18:39:47 -0300

    :rocket: [feat] Teste MediaPipe

---
+++ index.js
@@ -0,0 +1,69 @@
+function onResults(results) {
+
+  const canvasCtx = canvasElement.getContext('2d');
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage( results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks && results.multiHandedness) {
+    for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+      const classification = results.multiHandedness[index];
+      const isRightHand = classification.label === 'Right';
+      const landmarks = results.multiHandLandmarks[index];
+      if (showTracking.checked) {
+        drawConnectors(
+          canvasCtx, landmarks, HAND_CONNECTIONS,
+          {color: isRightHand ? '#fff' : '#056df5'}),
+      drawLandmarks(canvasCtx, landmarks, {
+        color: isRightHand ? '#fff' : '#056df5',
+        fillColor: isRightHand ? '#056df5' : '#fff',
+        radius: (x) => {
+          return lerp(x.from.z, -0.15, .1, 10, 1);
+        }
+      })};
+
+    if (isRightHand === false){
+        leftIndex = landmarks[8];
+        leftWrist = landmarks[0];
+        if(est.gestures.length) {
+          leftGesture = est.gestures[0].name;
+          } else {
+            leftGesture = "noGesture"
+          };
+        } else {
+        rightIndex = landmarks[8];
+        rightWrist = landmarks[0];
+        if(est.gestures.length) {
+          rightGesture = est.gestures[0].name;
+          } else {
+            rightGesture = "noGesture"
+          };
+      }
+    canvasCtx.restore();
+    };
+  };
+}
+
+
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+
+hands.setOptions({
+  selfieMode: true,
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+
+hands.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
